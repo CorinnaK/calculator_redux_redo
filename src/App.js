68632aaf1e1dbc1f048f3e86fd4984b1b131228c
CorinnaK <corinna.kelly@outlook.com>
2020-07-27 22:41:30 -0600

    added background image and some css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
       num1: "", // Keep track of value in first input
       num2: "", // Keep track of value in second input
       oper: "+", // Keep track of what operand is selected
-      result: "",
+      result: "", // Keep track of results to display later
     };
   }
   updateCalc(key, value) {
@@ -20,7 +20,7 @@ class App extends React.Component {
     const num1 = Number(this.state.num1);
     const num2 = Number(this.state.num2);
     const oper = this.state.oper;
-
+    // Used if statements here, could not get switch to work, need more guidance
     if (oper === "+") {
       this.setState({ result: num1 + num2 });
     } else if (oper === "-") {
@@ -35,39 +35,41 @@ class App extends React.Component {
     return (
       <>
         <h1>Welcome to my calculator</h1>
-        <form onSubmit={this.calcResult}>
-          <label htmlFor="num1">First Number Input:</label>
-          <input
-            type="number"
-            name="num1"
-            id="num1"
-            required
-            value={this.state.num1}
-            onChange={(event) => this.updateCalc("num1", event.target.value)}
-          />
-          <label htmlFor="oper">Operation:</label>
-          <select
-            value={this.state.oper}
-            onChange={(event) => this.updateCalc("oper", event.target.value)}
-          >
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="/">/</option>
-            <option value="*">*</option>
-          </select>
-          <label htmlFor="num2">First Number Input:</label>
-          <input
-            type="number"
-            id="num2"
-            required
-            value={this.state.num2}
-            onChange={(event) => this.updateCalc("num2", event.target.value)}
-          />
-          <input type="submit" value="Calculate" />
-        </form>
-        <div>
-          <h1>Result = {this.state.result}</h1>
-          <p></p>
+        <div className="container">
+          <form onSubmit={this.calcResult}>
+            <label htmlFor="num1">First Number Input:</label>
+            <input
+              type="number"
+              name="num1"
+              id="num1"
+              required
+              value={this.state.num1}
+              onChange={(event) => this.updateCalc("num1", event.target.value)}
+            />
+            <label htmlFor="oper">Operation:</label>
+            <select
+              value={this.state.oper}
+              onChange={(event) => this.updateCalc("oper", event.target.value)}
+            >
+              <option value="+">+</option>
+              <option value="-">-</option>
+              <option value="/">/</option>
+              <option value="*">*</option>
+            </select>
+            <label htmlFor="num2">Second Number Input:</label>
+            <input
+              type="number"
+              id="num2"
+              required
+              value={this.state.num2}
+              onChange={(event) => this.updateCalc("num2", event.target.value)}
+            />
+            <input type="submit" id="submit" value="Calculate" />
+          </form>
+          <div>
+            <h1>Result = {this.state.result}</h1>
+            <p></p>
+          </div>
         </div>
       </>
     );
